68b6136f533c690db4ee6ce5025f21405dabae21
Olaniyan Foluso <olaniyanfoluso498@gmail.com>
2020-07-29 04:52:17 +0100

    Updates

---
+++ precache-manifest.ecb06e26361c7943c87a25ae09792a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fdda4df1d6aa9cf14d641d6453a797c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "dd02073331f0e4a3be04",
+    "url": "/monsters/static/css/main.94497522.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd02073331f0e4a3be04",
+    "url": "/monsters/static/js/main.78d00c23.chunk.js"
+  },
+  {
+    "revision": "55328b6a8e96452d341b",
+    "url": "/monsters/static/js/runtime-main.3a250f91.js"
+  }
+]);
